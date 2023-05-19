ae898c0c96f86681de3a0f12a71bfafb91daca4b
mcdowellster <95304554+mcdowellster@users.noreply.github.com>
2023-05-19 14:06:49 -0400

    Fixed OT config

--- GIM/scripts/gim_changes.sh
+++ GIM/scripts/gim_changes.sh
@@ -2,5 +2,5 @@
 sudo apt install ncat -y
 
 #Update OT Config
-mv /home/ubuntu/GIT/GIM/configs/olivetin/config.yml /OliveTin-linux-amd64/config.yml
+mv /home/ubuntu/GIT/GIM/configs/olivetin/config.yaml /OliveTin-linux-amd64/config.yaml
 sudo systemctl restart OliveTin.service
