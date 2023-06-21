e061412826e4a6b8454cb81897edfb23d17273ed
BlueTeamNinja <1891183+BlueTeamNinja@users.noreply.github.com>
2023-06-21 16:12:52 -0400

    Ports suck

--- Graylog - CTFd/scripts/config.sh
+++ Graylog - CTFd/scripts/config.sh
@@ -1,6 +1,6 @@
 #Load Abe's SwapShop!
 docker run -p 7000:80 -d --restart always --log-driver gelf --log-opt gelf-address=tcp://localhost:12201 blueteamninja/swagshop
-docker run -p 8080:80 -d --restart always tarampampam/webhook-tester
+docker run -p 8080:8080 -d --restart always tarampampam/webhook-tester
 
 #Ricks Sysadmin stuffs!
 
