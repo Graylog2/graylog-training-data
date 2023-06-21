a45ee41d729305da4438b219379bed1dc3fc9204
mcdowellster <mcdowellster@gmail.com>
2023-06-20 20:10:00 -0400

    humble beginnings

--- Graylog - CTFd/scripts/config.sh
+++ Graylog - CTFd/scripts/config.sh
@@ -1,1 +1,6 @@
-echo "Base Config File for universal framework"
+#Load Abe's SwapShop!
+docker run -p 7000:80 -d --restart always --log-driver gelf --log-opt gelf-address=tcp://localhost:12201 blueteamninja/swagshop
+
+#Ricks Sysadmin stuffs!
+
+#Dan's Log magic goes here
