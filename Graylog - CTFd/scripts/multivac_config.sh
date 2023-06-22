e6a8794c11f78f50e3b4d7705cfa9d6814e94903
a-zombie <119435230+a-zombie@users.noreply.github.com>
2023-06-21 23:11:23 -0400

    more flagging for a flogging

--- Graylog - CTFd/scripts/multivac_config.sh
+++ Graylog - CTFd/scripts/multivac_config.sh
@@ -1,3 +1,5 @@
+apt-get update
+apt-get install -y wget
 wget https://packages.graylog2.org/repo/packages/graylog-sidecar-repository_1-5_all.deb
 dpkg -i graylog-sidecar-repository_1-5_all.deb
 wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo tee -a /etc/apt/trusted.gpg.d/elasticsearch.asc
@@ -9,7 +11,12 @@ apt-get install -y filebeat
 sed -i 's/^#PermitRootLogin prohibit-password/PermitRootLogin yes/' /etc/ssh/sshd_config
 systemctl restart ssh
 echo 'root:AlexanderAdell' | chpasswd
-sed -i  's/^
+sed -i '1s/^/\n/' /etc/graylog/sidecar/sidecar.yml
+sed -i '1s/^/#Have a flag: oN-a-sPace-sHip-tO-X-23\n/' /etc/graylog/sidecar/sidecar.yml
+sed -i '1s/^/#Who can remember all these config file names? I guess since you figured it out you can\n/' /etc/graylog/sidecar/sidecar.yml
+sed -i 's/^#tls_skip_verify: false/tls_skip_verify: true/' /etc/graylog/sidecar/sidecar.yml
+sed -i 's|^#server_url: "http://127.0.0.1:9000/api/"|server_url: "https://graylog:443/api/|' /etc/graylog/sidecar/sidecar.yml
+sed -i 's/server_api_token: ""/server_api_token: "ZZZZZTOKENTOKENZZZZZ"/' /etc/graylog/sidecar/sidecar.yml
 touch /var/log/thelastquestion.log
 echo 'printf "You found the right system, here is a flag: CosmicACOneDay!\n"' >> /root/.bashrc
 (crontab -l 2>/dev/null; echo */1 \* \* \* \* echo \'RG91Z1RoZUdpZ2FudGljUGlwZXI=\' \>\> /var/log/thelastquestion.log) | crontab -
