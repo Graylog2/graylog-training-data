62e20e09e917c869030b560985353a072b38a031
William Trelawny <william.trelawny@graylog.com>
2023-06-27 19:30:51 -0400

    updated hostname vars & cert magic

--- graylog_tls/scripts/generate-csrs.sh
+++ graylog_tls/scripts/generate-csrs.sh
@@ -10,15 +10,15 @@ done
 while true
 do
     read -p "Hostname of Opensearch server: " i
-    [ ! $i == "$user-opensearch.logfather.org" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $user-opensearch.logfather.org!)"
-    [[ $i == "$user-opensearch.logfather.org" ]] && break
+    [ ! $i == "$publicdns_os" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $publicdns_os!)"
+    [[ $i == "$publicdns_os" ]] && break
 done
 
 while true
 do
     read -p "Hostname of MongoDB server: " i
-    [ ! $i == "$user-mongodb.logfather.org" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $user-mongodb.logfather.org!)"
-    [[ $i == "$user-mongodb.logfather.org" ]] && break
+    [ ! $i == "$publicdns_mg" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $publicdns_mg!)"
+    [[ $i == "$publicdns_mg" ]] && break
 done
 
 echo "=== Generating CSR from provided info..."
@@ -27,10 +27,19 @@ echo "=== Submitting CSR to CA for signing..."
 sleep 3
 
 mkdir ~/ssl
-sudo cp /home/admin/.class/certs/cert.pem /home/admin/.class/certs/privkey.pem /home/admin/.class/certs/fullchain.pem ~/ssl
+cd ~/ssl
+sudo cp /home/admin/.class/certs/* .
 sudo chown 1000.1000 -R ~/ssl
+cp cert.pem graylog.pem
+cp cert.pem mongodb.pem
+cp privkey.pem graylog.key
+cp privkey.pem mongodb.key
+rm cert.pem privkey.pem fullchain.pem
 echo "=== Certificate signature succeeded!"
-echo "=== Your certificate, private key, and full cert chain have been uploaded to the ~/ssl directory:"
-echo "=== Server cert      : ~/ssl/cert.pem"
-echo "=== Server key       : ~/ssl/privkey.pem"
-echo "=== Cert trust chain : ~/ssl/fullchain.pem"
+echo "=== Your CA certificates, server certificates, and private keys have been uploaded to the $HOME/ssl directory:"
+echo "=== Graylog server cert     : $HOME/ssl/graylog.pem"
+echo "=== Graylog server key      : $HOME/ssl/graylog.key"
+echo "=== MongoDB server cert     : $HOME/ssl/mongodb.pem"
+echo "=== MongoDB server key      : $HOME/ssl/mongodb.key"
+echo "=== Intermediate CA cert    : $HOME/ssl/intermediate-ca.pem"
+echo "=== Root CA cert            : $HOME/ssl/root-ca.pem"
