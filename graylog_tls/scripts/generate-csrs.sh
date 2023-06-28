90cf908d9dce2f0274c9eaf0a0f681ecb03881e7
William Trelawny <william.trelawny@graylog.com>
2023-06-28 14:59:36 -0400

    added cert gen for opensearch (duh)

--- graylog_tls/scripts/generate-csrs.sh
+++ graylog_tls/scripts/generate-csrs.sh
@@ -9,22 +9,23 @@ done
 
 while true
 do
-    read -p "Hostname of Opensearch server: " i
-    [ ! $i == "$publicdns_os" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $publicdns_os!)"
-    [[ $i == "$publicdns_os" ]] && break
-done
-
-while true
-do
     read -p "Hostname of MongoDB server: " i
     [ ! $i == "$publicdns_mg" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $publicdns_mg!)"
     [[ $i == "$publicdns_mg" ]] && break
 done
 
+while true
+do
+    read -p "Hostname of Opensearch server: " i
+    [ ! $i == "$publicdns_os" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $publicdns_os!)"
+    [[ $i == "$publicdns_os" ]] && break
+done
+
+
 echo "=== Generating CSR from provided info..."
-sleep 1
+sleep 2
 echo "=== Submitting CSR to CA for signing..."
-sleep 3
+sleep 4
 
 mkdir ~/ssl
 cd ~/ssl
@@ -32,8 +33,10 @@ sudo cp /home/admin/.class/certs/* .
 sudo chown 1000.1000 -R ~/ssl
 cp cert.pem graylog.pem
 cp cert.pem mongodb.pem
+cp cert.pem opensearch.pem
 cp privkey.pem graylog.key
 cp privkey.pem mongodb.key
+cp privkey.pem opensearch.key
 rm cert.pem privkey.pem fullchain.pem
 echo "=== Certificate signature succeeded!"
 echo "=== Your CA certificates, server certificates, and private keys have been uploaded to the $HOME/ssl directory:"
@@ -41,5 +44,7 @@ echo "=== Graylog server cert     : $HOME/ssl/graylog.pem"
 echo "=== Graylog server key      : $HOME/ssl/graylog.key"
 echo "=== MongoDB server cert     : $HOME/ssl/mongodb.pem"
 echo "=== MongoDB server key      : $HOME/ssl/mongodb.key"
+echo "=== MongoDB server cert     : $HOME/ssl/opensearch.pem"
+echo "=== MongoDB server key      : $HOME/ssl/opensearch.key"
 echo "=== Intermediate CA cert    : $HOME/ssl/intermediate-ca.pem"
 echo "=== Root CA cert            : $HOME/ssl/root-ca.pem"
