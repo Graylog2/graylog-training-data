4a38b391f745864f773b66b36e17b2508aa2166e
mcdowellster <mcdowellster@gmail.com>
2023-07-06 23:35:05 -0400

    common scripts

---
+++ common/cleanup.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#load Vars from Strigo
+source /etc/profile
+
+#Cleanup
+echo "Cleaning up" >> /home/ubuntu/strigosuccess
+sed -i '/export apitoken=/d' /etc/profile
+sed -i '/export authemail=/d' /etc/profile
+rm -r /certs
+rm -r /$STRIGO_CLASS_ID
+
+echo "Complete!" >> /home/ubuntu/strigosuccess
