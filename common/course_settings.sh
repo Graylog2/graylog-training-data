2002cdc04fc3dfde4b81ecc22bc12569cfd3d008
mcdowellster <mcdowellster@gmail.com>
2023-07-12 13:31:03 -0400

    Fixed maxmind settings

--- common/course_settings.sh
+++ common/course_settings.sh
@@ -4,8 +4,7 @@ source /etc/profile
 
 #Update MaxMind DA with updated DB Path
 echo "Updating MaxMind DA" >> /home/$LUSER/strigosuccess
-id=$(curl -u 'admin:yabba dabba doo' -XGET 'http://localhost:9000/api/system/lookup/adapters?page=1&per_page=50&sort=title&order=desc&query=Geo' | jq -r '.data_adapters[].id')
-curl -u 'admin:yabba dabba doo' -XPUT "http://localhost:9000/api/system/lookup/adapters/$id" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d "{\"id\":\"$id\",\"title\":\"Geo IP - MaxMind™ Databases\",\"description\":\"Geo IP - MaxMind™ Databases\",\"name\":\"geo-ip-maxmind\",\"custom_error_ttl_enabled\":false,\"custom_error_ttl\":null,\"custom_error_ttl_unit\":null,\"config\":{\"type\":\"maxmind_geoip\",\"path\":\"/usr/share/graylog/data/config/GeoLite2-City.mmdb\",\"database_type\":\"MAXMIND_CITY\",\"check_interval\":1,\"check_interval_unit\":\"MINUTES\"}}" >> /home/$LUSER/strigosuccess
+curl -u 'admin:yabba dabba doo' -XPUT "http://localhost:9000/api/system/cluster_config/org.graylog.plugins.map.config.GeoIpResolverConfig" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"enabled":true,"enforce_graylog_schema":true,"db_vendor_type":"MAXMIND","city_db_path":"/usr/share/graylog/data/config/GeoLite2-City.mmdb","asn_db_path":"/usr/share/graylog/data/config/GeoLite2-ASN.mmdb","refresh_interval_unit":"MINUTES","refresh_interval":10,"use_s3":false}' >> /home/$LUSER/strigosuccess
 
 #Pausing Endpoint Firewalls Stream
 printf "\n\nRetrieve Endpoint Firewalls Stream ID\n" >> /home/$LUSER/strigosuccess
