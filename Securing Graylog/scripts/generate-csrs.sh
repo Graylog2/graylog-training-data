0522f3ba7b69a149d595c230911df91f56a80796
BlueTeamNinja <1891183+BlueTeamNinja@users.noreply.github.com>
2023-07-19 17:32:30 -0400

    COLOURS.  ALWAYS COLOURS.  :D

--- Securing Graylog/scripts/generate-csrs.sh
+++ Securing Graylog/scripts/generate-csrs.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-while true
-do
-    read -p "Hostname of Graylog server: " i
-    [ ! $i == "$publicdns" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $publicdns!)"
-    [[ $i == "$publicdns" ]] && break
-done
-
-while true
-do
-    read -p "Hostname of Opensearch server: " i
-    [ ! $i == "$dns_os" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $dns_os!)"
-    [[ $i == "$dns_os" ]] && break
-done
+# while true
+# do
+#     read -p "Hostname of Graylog server: " i
+#     [ ! $i == "$publicdns" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $publicdns!)"
+#     [[ $i == "$publicdns" ]] && break
+# done
+# 
+# while true
+# do
+#     read -p "Hostname of Opensearch server: " i
+#     [ ! $i == "$dns_os" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $dns_os!)"
+#     [[ $i == "$dns_os" ]] && break
+# done
 
 
 echo "=== Generating CSR's from provided info..."
@@ -31,11 +31,11 @@ cp privkey.pem graylog.key
 cp privkey.pem opensearch.key
 chmod 0400 ./*.key
 rm cert.pem privkey.pem
-echo "=== Certificates signature succeeded!"
-echo "=== Your CA certificates, server certificates, and private keys have been uploaded to $HOME/ssl:"
-echo "=== Graylog server cert     : $HOME/ssl/graylog.pem"
-echo "=== Graylog server key      : $HOME/ssl/graylog.key"
-echo "=== Opensearch server cert  : $HOME/ssl/opensearch.pem"
-echo "=== Opensearch server key   : $HOME/ssl/opensearch.key"
-echo "=== Intermediate CA cert    : $HOME/ssl/intermediate-ca.pem"
-echo "=== Root CA cert            : $HOME/ssl/root-ca.pem"
+printf "=== Certificates signature succeeded!\n"
+printf "=== Your CA certificates, server certificates, and private keys have been uploaded to $HOME/ssl:\n"
+printf "=== Graylog server cert     :\e[0;32m $HOME/ssl/graylog.pem\e[0;37m\n"
+printf "=== Graylog server key      :\e[1;32m $HOME/ssl/graylog.key\e[0;37m\n"
+printf "=== Opensearch server cert  :\e[0;33m $HOME/ssl/opensearch.pem\e[0;37m\n"
+printf "=== Opensearch server key   :\e[1;33m $HOME/ssl/opensearch.key\e[0;37m\n"
+printf "=== Intermediate CA cert    :\e[0;36m $HOME/ssl/intermediate-ca.pem\e[0;37m\n"
+printf "=== Root CA cert            :\e[1;36m $HOME/ssl/root-ca.pem\e[0;37m\n"
