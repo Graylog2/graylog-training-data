92dc214eb0da12c7c898caa06edf4bfce16acb03
BlueTeamNinja <1891183+BlueTeamNinja@users.noreply.github.com>
2023-07-20 09:44:52 -0400

    COLOURS

--- 64384cec22c8f6a4e5a0c6ec/scripts/generate_certs.sh
+++ 64384cec22c8f6a4e5a0c6ec/scripts/generate_certs.sh
@@ -6,7 +6,7 @@
 #     [ ! $i == "$publicdns" ] && echo "Please make sure this name matches the full domain name of your lab instance! (Hint: it's $publicdns!)"
 #     [[ $i == "$publicdns" ]] && break
 # done
-
+# 
 # while true
 # do
 #     read -p "Hostname of Opensearch server: " i
@@ -22,21 +22,21 @@ sleep 4
 
 mkdir $HOME/ssl
 cd $HOME/ssl
-sudo cp /etc/graylog/*.pem $HOME/ssl
-sudo rm /etc/graylog/*.pem
-sudo chown $LUSER.$LUSER -R $HOME/ssl
+cp /certs/*.pem .
+sudo rm -rf /certs
+sudo chown admin.admin -R $HOME/ssl
 cp cert.pem graylog.pem
 cp cert.pem opensearch.pem
 cp privkey.pem graylog.key
 cp privkey.pem opensearch.key
-chmod 0600 ./*
+chmod 0400 ./*.key
 rm cert.pem privkey.pem
-echo "=== Certificates signature succeeded!"
-echo "=== Your CA certificates, server certificates, and private keys have been uploaded to $HOME/ssl:"
-echo "=== Graylog server cert     : $HOME/ssl/graylog.pem"
-echo "=== Graylog server key      : $HOME/ssl/graylog.key"
-echo "=== Opensearch server cert  : $HOME/ssl/opensearch.pem"
-echo "=== Opensearch server key   : $HOME/ssl/opensearch.key"
-echo "=== Intermediate CA cert    : $HOME/ssl/intermediate-ca.pem"
-echo "=== Root CA cert            : $HOME/ssl/root-ca.pem"
-echo "=== Full certificate chain  : $HOME/ssl/fullchain.pem"
+
+printf "=== Certificates signature succeeded!\n"
+printf "=== Your CA certificates, server certificates, and private keys have been uploaded to $HOME/ssl:\n"
+printf "=== Graylog server cert     :\e[0;32m $HOME/ssl/graylog.pem\e[0;37m\n"
+printf "=== Graylog server key      :\e[1;32m $HOME/ssl/graylog.key\e[0;37m\n"
+printf "=== Opensearch server cert  :\e[0;33m $HOME/ssl/opensearch.pem\e[0;37m\n"
+printf "=== Opensearch server key   :\e[1;33m $HOME/ssl/opensearch.key\e[0;37m\n"
+printf "=== Intermediate CA cert    :\e[0;36m $HOME/ssl/intermediate-ca.pem\e[0;37m\n"
+printf "=== Root CA cert            :\e[1;36m $HOME/ssl/root-ca.pem\e[0;37m\n"
