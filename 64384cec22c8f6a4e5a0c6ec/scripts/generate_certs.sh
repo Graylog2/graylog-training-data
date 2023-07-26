5a11da306323b6813584d9e32b49243eeb7d64b3
William Trelawny <william.trelawny@graylog.com>
2023-07-26 16:40:46 -0400

    forgot to include osadmin fullchain
    - also renamed files
    
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- 64384cec22c8f6a4e5a0c6ec/scripts/generate_certs.sh
+++ 64384cec22c8f6a4e5a0c6ec/scripts/generate_certs.sh
@@ -11,9 +11,10 @@ cp cert.pem /home/$LUSER/certs/graylog.pem
 cp cert.pem /home/$LUSER/certs/opensearch.pem
 cp privkey.pem /home/$LUSER/certs/graylog.key
 cp privkey.pem /home/$LUSER/certs/opensearch.key
-cp fullchain.pem /home/$LUSER/certs/servers_fullchain.pem
+cp fullchain.pem /home/$LUSER/certs/fullchain.pem
 cp osadmin_cert.pem /home/$LUSER/certs/osadmin.pem
 cp osadmin_privkey.pem /home/$LUSER/certs/osadmin.key
+cp osadmin_fullchain.pem /home/$LUSER/certs/osadmin_fullchain.pem
 chmod 0400 /home/$LUSER/certs/*.key
 sudo rm -rf /.ssl
 
@@ -23,7 +24,7 @@ printf "      \e[0;31mGraylog server cert ......... graylog.pem\e[0m\n"
 printf "      \e[0;31mGraylog server key .......... graylog.key\e[0m\n"
 printf "      \e[0;34mOpensearch server cert ...... opensearch.pem\e[0m\n"
 printf "      \e[0;34mOpensearch server key ....... opensearch.key\e[0m\n"
-printf "      \e[0;33mCertificate chain ........... servers_fullchain.pem\e[0m\n\n"
+printf "      \e[0;33mCertificate chain ........... fullchain.pem\e[0m\n\n"
 printf "      \e[0;36mOpensearch admin user cert .. osadmin.pem\e[0m\n"
 printf "      \e[0;36mOpensearch admin user key ... osadmin.key\e[0m\n"
 printf "      \e[0;33mOpensearch admin fullchain .. osadmin_fullchain.pem\e[0m\n"
