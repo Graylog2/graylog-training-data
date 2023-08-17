939170d8b230ed77b6f5d65ba9e78fe2b4b429a0
mcdowellster <mcdowellster@gmail.com>
2023-08-17 16:22:31 -0400

    Cleanup

--- common/ot_gl_theme.sh
+++ common/ot_gl_theme.sh
@@ -4,13 +4,13 @@ source /etc/profile
 echo "Running GL OT Theming Script" >> /home/$LUSER/strigosuccess
 
 #Tarball Install
-DIR="/var/www/olivetin/themes"
-if [ -d "$DIR" ]; then
-    echo "Installing gl ot theme in ${DIR}..." >> /home/$LUSER/strigosuccess
-    mv ./common/graylog-theme/ $DIR
-fi
+#DIR="/var/www/olivetin/themes"
+#if [ -d "$DIR" ]; then
+#    echo "Installing gl ot theme in ${DIR}..." >> /home/$LUSER/strigosuccess
+#    mv ./common/graylog-theme/ $DIR
+#fi
 
-#Deb Install
+#Deb/Tar Install - SYMB Links made to make this the same
 DIR="/etc/OliveTin/webui/themes/"
 if [ -d "$DIR" ]; then
     echo "Installing gl ot theme in ${DIR}..." >> /home/$LUSER/strigosuccess
