8942b3c194548618a631e3b89c4a9d0a6935c393
mcdowellster <mcdowellster@gmail.com>
2023-09-01 16:21:31 -0400

    Edward's Changes

--- 64654766bfb7a1b37088702d/scripts/config_common.sh
+++ 64654766bfb7a1b37088702d/scripts/config_common.sh
@@ -39,6 +39,7 @@ sudo systemctl restart OliveTin.service >> /home/$LUSER/strigosuccess
 #Update Docker Config for new OT Port
 echo "Adding required inputs to GL DC" >> /home/$LUSER/strigosuccess
 sed -i '/^      - "12201:12201\/udp" # GELF UDP.*/a\      - "5555:5555\/tcp"   # Raw TCP' /etc/graylog/docker-compose-glservices.yml
+sed -i '/^      - "12201:12201\/udp" # GELF UDP.*/a\      - "5140:5140\/tcp"   # Syslog TCP' /etc/graylog/docker-compose-glservices.yml
 
 #Update Graylog Container
 echo "Restarting GL Docker to reflect changes" >> /home/$LUSER/strigosuccess
@@ -52,7 +53,8 @@ done
 
 #Add TCP Raw input
 echo "Adding required inputs to GL" >> /home/$LUSER/strigosuccess
-curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.raw.tcp.RawTCPInput","configuration":{"bind_address":"0.0.0.0","port":5555,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":false,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8"},"title":"PiHole Data","global":true,"node":"93f01a3f-d051-436f-9bab-0c11f22cd55c"}'
+#curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.raw.tcp.RawTCPInput","configuration":{"bind_address":"0.0.0.0","port":5555,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":false,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8"},"title":"PiHole Data","global":true,"node":"93f01a3f-d051-436f-9bab-0c11f22cd55c"}'
+
 
 #Move Log Data
 echo "Moving Log Data" >> /home/$LUSER/strigosuccess
