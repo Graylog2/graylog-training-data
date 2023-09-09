d6d51ab17a802172696874441dedf8ed92cf36e3
William Trelawny <william.trelawny@graylog.com>
2023-09-08 20:07:01 -0400

    major rewrite config_common.sh
    - Updated to accomodate non-Dan-AMI and non-Docker envs
    - rm COMMON_SKIP_* vars, instead baked logic into scripts as needed
    - added touch ~/gogogo here since every course should utilize this mech
    and this won't break courses that don't.
    
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- 64fba6faa1cef99a176af41e/scripts/config_common.sh
+++ 64fba6faa1cef99a176af41e/scripts/config_common.sh
@@ -6,43 +6,42 @@ echo "Importing common scripts" >> /home/$LUSER/strigosuccess
 git svn clone "https://github.com/Graylog2/graylog-training-data/trunk/common" >> /home/$LUSER/strigosuccess
 chmod +x /common/*.sh
 
-# Skip execution of common scripts (set var to anything to register skip):
-# Note that dns.sh cannot be skipped as it is required for every course.
-COMMON_SKIP_CERTS=1
-COMMON_SKIP_INST_ILLUMINATE=
-COMMON_SKIP_COURSE_SETTINGS=
-COMMON_SKIP_CP_INST=
-COMMON_SKIP_DOCKER_CHG=
-
 #DNS
 ./common/dns.sh >> /home/$LUSER/strigosuccess
 
 #Cert Update
 ./common/certs.sh >> /home/$LUSER/strigosuccess
 
+# Install Graylog if NOT running a Docker env:
+if [ ! $(which docker) ]; then
+    ./common/inst_graylog.sh >> /home/$LUSER/strigosuccess
+fi
+
+#Illuminate Install
+./common/inst_illuminate.sh >> /home/$LUSER/strigosuccess
+
 #Course Settings
 ./common/course_settings.sh >> /home/$LUSER/strigosuccess
 
+#Add course Content Packs:
+./common/cp_inst.sh >> /home/$LUSER/strigosuccess
+
 #Update GL Docker Environment
 ## After this point everything will be HTTPS
-./common/docker_chg.sh >> /home/$LUSER/strigosuccess
-
-#Launch Docker to load changes in env file
-#echo "Running Docker Compose to update GL environment with new information" >> /home/$LUSER/strigosuccess
-#docker compose -f /etc/graylog/docker-compose-glservices.yml --env-file /etc/graylog/strigo-graylog-training-changes.env up -d
+if [ $(which docker) ]; then
+    ./common/docker_chg.sh >> /home/$LUSER/strigosuccess
+    #Launch Docker to load changes in env file
+    echo "Running Docker Compose to update GL environment with new information" >> /home/$LUSER/strigosuccess
+    docker compose -f /etc/graylog/docker-compose-glservices.yml --env-file /etc/graylog/strigo-graylog-training-changes.env up -d
+else
+    echo "Skipping execution of docker_chg.sh..." >> /home/$LUSER/strigosuccess
+fi
 
 #Run this to speed up first run with OliveTin
 pwsh -c 'write-host "loaded PS!"'
 
 # Import course-specific setup script:
-chmod +x ./$STRIGO_CLASS_ID/scripts/course_setup.sh
-./$STRIGO_CLASS_ID/scripts/course_setup.sh >> /home/$LUSER/strigosuccess
-
-#Add course Content Packs:
-./common/cp_inst.sh >> /home/$LUSER/strigosuccess
-
-#Illuminate Install
-./common/inst_illuminate.sh >> /home/$LUSER/strigosuccess
+./$STRIGO_CLASS_ID/scripts/config_custom.sh >> /home/$LUSER/strigosuccess
 
 #OT Theme
 ./common/ot_gl_theme.sh >> /home/$LUSER/strigosuccess
@@ -51,3 +50,7 @@ chmod +x ./$STRIGO_CLASS_ID/scripts/course_setup.sh
 ./common/cleanup.sh >> /home/$LUSER/strigosuccess
 
 echo "All setup complete!" >> /home/$LUSER/strigosuccess
+
+# Create file for lab to finally appear
+# (ref to Strigo init script)
+touch /home/$LUSER/gogogo
