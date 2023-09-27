32eed91d14ff9815852ff5d678181a3d871b4034
mcdowellster <mcdowellster@gmail.com>
2023-09-27 15:02:12 -0400

    OneM0rTime

--- 650de6ce3ae400b8e8ccd115/scripts/config_monitoring.sh
+++ 650de6ce3ae400b8e8ccd115/scripts/config_monitoring.sh
@@ -48,14 +48,42 @@ curl -X POST -H "Content-Type: application/json" -d @/etc/graylog/grafana/Elasti
 echo "Adding NGINX to slap headers in for grafana" >> /home/$LUSER/strigosuccess
 apt install nginx -y
 rm -r /etc/nginx/sites-enabled/default
-echo -e 'server {
+echo -e "server {
     listen 80;
     listen [::]:80;
 
     location / {
-        proxy_set_header Host $http_host;
+        proxy_set_header Host \$http_host;
         proxy_pass           http://localhost:3000/;
-    }
+    
+    if (\$request_method = 'OPTIONS') {
+        add_header 'Access-Control-Allow-Origin' '*';
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS';
+        #
+        # Custom headers and headers various browsers *should* be OK with but aren't
+        #
+        add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range';
+        #
+        # Tell client that this pre-flight info is valid for 20 days
+        #
+        add_header 'Access-Control-Max-Age' 1728000;
+        add_header 'Content-Type' 'text/plain; charset=utf-8';
+        add_header 'Content-Length' 0;
+        return 204;
+     }
+     if (\$request_method = 'POST') {
+        add_header 'Access-Control-Allow-Origin' '*' always;
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS' always;
+        add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range' always;
+        add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range' always;
+     }
+     if (\$request_method = 'GET') {
+        add_header 'Access-Control-Allow-Origin' '*' always;
+        add_header 'Access-Control-Allow-Methods' 'GET, POST, OPTIONS' always;
+        add_header 'Access-Control-Allow-Headers' 'DNT,User-Agent,X-Requested-With,If-Modified-Since,Cache-Control,Content-Type,Range' always;
+        add_header 'Access-Control-Expose-Headers' 'Content-Length,Content-Range' always;
+     }
+   }
 }
-' > /etc/nginx/sites-enabled/grafana.conf
+" > /etc/nginx/sites-enabled/grafana.conf
 systemctl restart nginx
