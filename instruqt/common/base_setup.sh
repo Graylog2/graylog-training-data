18999e3df5b93bb9851e4ff3015aca4fcfc14043
Dan McDowell <mcdowellster@gmail.com>
2023-10-30 14:34:57 -0400

    path typo

--- instruqt/common/base_setup.sh
+++ instruqt/common/base_setup.sh
@@ -82,7 +82,7 @@ sudo docker exec -i $mongoc /usr/bin/mongosh graylog --eval "db.cluster_config.u
 glc=$(sudo docker ps | grep graylog-enterprise | awk '{print $1}')
 sudo docker cp /common/geodb/GeoLite2-ASN.mmdb $glc:/usr/share/graylog/data/config/GeoLite2-ASN.mmdb
 sudo sudo docker exec -u root -i $glc chown graylog.graylog /usr/share/graylog/data/config/GeoLite2-ASN.mmdb
-sudo docker cp /common/geodbGeoLite2-City.mmdb $glc:/usr/share/graylog/data/config/GeoLite2-City.mmdb
+sudo docker cp /common/geodb/GeoLite2-City.mmdb $glc:/usr/share/graylog/data/config/GeoLite2-City.mmdb
 sudo sudo docker exec -u root -i $glc chown graylog.graylog /usr/share/graylog/data/config/GeoLite2-City.mmdb
 sudo docker cp /common/geodb/GeoLite2-Country.mmdb $glc:/usr/share/graylog/data/config/GeoLite2-Country.mmdb
 sudo sudo docker exec -u root -i $glc chown graylog.graylog /usr/share/graylog/data/config/GeoLite2-Country.mmdb
