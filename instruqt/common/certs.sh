73a81f7892f48fbdae0cec7f12e24c7d7abd5fe2
William Trelawny <william.trelawny@graylog.com>
2023-12-07 16:42:10 -0500

    strigo -> instruqt change
    - Changed initial $CLASS check to use "tls" instead of Strigo class ID
    
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- instruqt/common/certs.sh
+++ instruqt/common/certs.sh
@@ -3,7 +3,7 @@
 source /etc/profile
 
 # Skip execution for TLS course:
-if [[ "$CLASS" == "64384cec22c8f6a4e5a0c6ec" ]]; then
+if [[ "$CLASS" == "tls" ]]; then
   echo "Skipping execution of $(basename "$0") because this is the TLS course..." 
   exit
 fi
