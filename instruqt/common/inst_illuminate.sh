8336c89fa3491ba35a6896a1130d8e1a73dc2ab1
Dan McDowell <mcdowellster@gmail.com>
2023-12-07 20:02:26 -0500

    updated illuminate to run https

--- instruqt/common/inst_illuminate.sh
+++ instruqt/common/inst_illuminate.sh
@@ -4,9 +4,9 @@ source /etc/profile
 
 #Setup Illuminate using API
 printf "\n\nInstalling Illuminate" 
-ilver=$(curl -u 'admin:yabba dabba doo' -XGET 'http://localhost:9000/api/plugins/org.graylog.plugins.illuminate/bundles/hub/latest' | jq -r '.version')
+ilver=$(curl -u 'admin:yabba dabba doo' -XGET -k 'https://localhost/api/plugins/org.graylog.plugins.illuminate/bundles/hub/latest' | jq -r '.version')
 printf "\n\nFound Illuminate Version:$ilver\n" 
-ilinst=$(curl -u 'admin:yabba dabba doo' -XPOST "http://localhost:9000/api/plugins/org.graylog.plugins.illuminate/bundles/hub/$ilver" -H 'X-Requested-By: PS_TeamAwesome')
+ilinst=$(curl -u 'admin:yabba dabba doo' -XPOST -k "https://localhost/api/plugins/org.graylog.plugins.illuminate/bundles/hub/$ilver" -H 'X-Requested-By: PS_TeamAwesome')
 printf "\n\nDownload Version $ilver - result: $ilinst\n" 
-bunact=$(curl -u 'admin:yabba dabba doo' -XPOST "http://localhost:9000/api/plugins/org.graylog.plugins.illuminate/bundles/$ilver" -H 'X-Requested-By: PS_TeamAwesome')
+bunact=$(curl -u 'admin:yabba dabba doo' -XPOST -k "https://localhost/api/plugins/org.graylog.plugins.illuminate/bundles/$ilver" -H 'X-Requested-By: PS_TeamAwesome')
 printf "\n\nInstallation Result: $bunact\n" 
