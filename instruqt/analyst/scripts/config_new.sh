28aaee1a877ed89725037b79590069bd2545f4db
William Trelawny <william.trelawny@graylog.com>
2023-12-12 14:56:11 -0500

    Modified per Framework modularization initiative
    - Added Index Set creation
    - Moved dns.sh execution to setup_common.sh
    - Did not touch existing config.sh since there are live classes
    
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

---
+++ instruqt/analyst/scripts/config_new.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Import env vars used throughout scripts runtime
+source /etc/profile
+
+#LogData
+sudo mv /$CLASS/log_data/* /root/powershell/Data
+sudo chown -R root.root /root
+mkdir /home/ubuntu/pipeline_rules
+sudo mv /$CLASS/pipeline_rules/* /home/ubuntu/pipeline_rules
+
+#Creating Indices
+printf "\n\ncreate General Desktop Events Index\n"
+curl -u 'admin:yabba dabba doo' -XPOST 'http://localhost:9000/api/system/indices/index_sets' -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"title":"General Desktop Events","description":"General Desktop Events","index_prefix":"general-desktop","writable":true,"can_be_default":true,"shards":2,"replicas":0,"retention_strategy_class":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategy","retention_strategy":{"max_number_of_indices":3,"type":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategyConfig"},"index_analyzer":"standard","index_optimization_max_num_segments":1,"index_optimization_disabled":false,"field_type_refresh_interval":5000,"rotation_strategy_class":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategy","rotation_strategy":{"max_docs_per_index":20000,"type":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategyConfig"},"creation_date":"2022-08-17T21:06:47.393Z"}'
+printf "\n\ncreate Training Index\n"
+curl -u 'admin:yabba dabba doo' -XPOST 'http://localhost:9000/api/system/indices/index_sets' -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"title":"Training","description":"Training","index_prefix":"train","writable":true,"can_be_default":true,"shards":2,"replicas":0,"retention_strategy_class":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategy","retention_strategy":{"max_number_of_indices":3,"type":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategyConfig"},"index_analyzer":"standard","index_optimization_max_num_segments":1,"index_optimization_disabled":false,"field_type_refresh_interval":5000,"rotation_strategy_class":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategy","rotation_strategy":{"max_docs_per_index":20000,"type":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategyConfig"},"creation_date":"2022-08-17T21:06:47.393Z"}'
+
+#Cert Injection
+/common/certs.sh
+
+#Course Settings
+/common/course_settings.sh 
+
+#Add course CPs
+/common/cp_inst.sh 
+
+#OT Theme
+/common/ot_gl_theme.sh 
+
+#Update GL Docker Environment
+## After this point everything will be HTTPS
+/common/docker_chg.sh
+
+#Illuminate Install - moved to POST docker update. Illuminate doesn't seem to fetch first time graylog runs
+/common/inst_illuminate.sh 
+
+#Cleanup
+/common/cleanup.sh 
+
+echo "Complete!" 
