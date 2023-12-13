eefb86afff34c8571510740a99a6b7e569511f93
William Trelawny <william.trelawny@graylog.com>
2023-12-13 17:50:39 -0500

    changed Graylog API check to allow non-auth checks
    - The check for Graylog API availability was failing bc it was using creds that did not exist yet.
    - To remove the creds, I also had to use an api endpoint (/api) that does not require any creds
    
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- instruqt/common/install_graylog.sh
+++ instruqt/common/install_graylog.sh
@@ -46,14 +46,14 @@ while ! curl -s localhost:9200
 do
     echo "Waiting for Opensearch API to come online before launching Graylog..."
     sleep 5
-done
+done > /dev/null
 
 systemctl enable --now graylog-server.service
 # Wait for Graylog to be accessible before continuing
-while ! curl -s -u 'admin:yabba dabba doo' http://localhost:9000/api/system/cluster/nodes; do
+while ! curl -s http://localhost:9000/api; do
 	printf "\n\nWaiting for Graylog to come online...\n"
     sleep 5
-done
+done > /dev/null
 
 # Set Graylog Cluster ID:
 /usr/bin/mongosh graylog --eval "db.cluster_config.updateMany({\"type\":\"org.graylog2.plugin.cluster.ClusterId\"}, {\$set:{payload:{cluster_id:\"$cluster_id\"}}});"
