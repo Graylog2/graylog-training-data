837ed9c7ca4d1e35f59f6ad1537c7fbdd4edbb05
William Trelawny <william.trelawny@graylog.com>
2023-12-13 15:13:21 -0500

    Update instruqt_track_setup.sh
    - Added $TITLE env var that contains "pretty" version of track name
    - Added $NEEDS_DOCKER env var for courses that need a Docker GL setup
    - improved comments
    - added hyphen to $dns env var
    
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- instruqt/common/instruqt_track_setup.sh
+++ instruqt/common/instruqt_track_setup.sh
@@ -1,14 +1,14 @@
 set -eo pipefail
 export DEBIAN_FRONTEND=noninteractive
 
-#Set License Details
+# Set License Details
 cluster_id=${cluster_id}
 license_enterprise=${license_enterprise}
 license_security=${license_security}
 gn_api_key=${gn_api_key}
 authemail=${cloudflare_auth_email}
 apitoken=${cloudflare_auth_token}
-dns=${HOSTNAME}${_SANDBOX_ID}
+dns=${HOSTNAME}-${_SANDBOX_ID}
 
 echo "export cluster_id=$cluster_id" >> /etc/profile
 echo "export license_enterprise=$license_enterprise" >> /etc/profile
@@ -18,24 +18,29 @@ echo "export apitoken=$apitoken" >> /etc/profile
 echo "export authemail=$authemail" >> /etc/profile
 echo "export dns=$dns" >> /etc/profile
 
-#Setup Class Information
+# Setup Class Information
 CLASS="analyst"
+TITLE="Graylog Analyst"
 echo "export CLASS=$CLASS" >> /etc/profile
+echo "export TITLE=$TITLE" >> /etc/profile
 
-#Cert Decode File
+# Uncomment below if this course needs a Graylog Docker environment:
+echo "export NEEDS_DOCKER=yes" >> /etc/profile
+
+# Cert Decode File
 echo "${cert_pwd}" > /root/.pwd
 
-#Base Apps
+# Base Apps
 printf "\n\nGrabbing Base Apps"
 sudo apt update
 sudo apt install git-svn -y
 
-#common Scripts
+# common Scripts
 printf "\n\nGrabbing Common Scripts"
 git svn clone "https://github.com/Graylog2/graylog-training-data/trunk/instruqt/common"
 sudo mv common /common
 sudo chmod +x /common/*.sh
 
-#Run Base Install
+# Run Base Install
 printf "\n\nRunning Base Install"
 /common/base_setup.sh
