579a1b6fa8acd9253e64ab1aebf1b5622807d767
William Trelawny <william.trelawny@graylog.com>
2023-12-13 14:31:43 -0500

    Changed directories per Ubuntu env needs
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- instruqt/tls/scripts/generate_certs.sh
+++ instruqt/tls/scripts/generate_certs.sh
@@ -6,20 +6,20 @@ echo "=== Submitting CSR's to CA for signing..."
 sleep 4
 
 # If certs dir does not exist, make it:
-[[ ! -d /home/$LUSER/certs ]] && mkdir /home/$LUSER/certs
+[[ ! -d /root/certs ]] && mkdir /root/certs
 cd /.ssl
-cp cert.pem /home/$LUSER/certs/graylog.pem
-cp cert.pem /home/$LUSER/certs/opensearch.pem
-cp privkey.pem /home/$LUSER/certs/graylog.key
-cp privkey.pem /home/$LUSER/certs/opensearch.key
-cp fullchain.pem /home/$LUSER/certs/fullchain.pem
-cp osadmin_cert.pem /home/$LUSER/certs/osadmin.pem
-cp osadmin_privkey.pem /home/$LUSER/certs/osadmin.key
-cp osadmin_fullchain.pem /home/$LUSER/certs/osadmin_fullchain.pem
-chmod 0400 /home/$LUSER/certs/*.key
+cp cert.pem /root/certs/graylog.pem
+cp cert.pem /root/certs/opensearch.pem
+cp privkey.pem /root/certs/graylog.key
+cp privkey.pem /root/certs/opensearch.key
+cp fullchain.pem /root/certs/fullchain.pem
+cp osadmin_cert.pem /root/certs/osadmin.pem
+cp osadmin_privkey.pem /root/certs/osadmin.key
+cp osadmin_fullchain.pem /root/certs/osadmin_fullchain.pem
+chmod 0400 /root/certs/*.key
 
 printf "\n\e[1;37m=== Certificates signed successfully!\e[0m\n"
-printf "\n=== Your certificate files & private keys have been uploaded to: \e[1;93m/home/$LUSER/certs\e[0m\n\n"
+printf "\n=== Your certificate files & private keys have been uploaded to: \e[1;93m/root/certs\e[0m\n\n"
 printf "      \e[0;31mGraylog server cert ......... graylog.pem\e[0m\n"
 printf "      \e[0;31mGraylog server key .......... graylog.key\e[0m\n"
 printf "      \e[0;34mOpensearch server cert ...... opensearch.pem\e[0m\n"
