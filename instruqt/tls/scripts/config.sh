edd5ef1a785cefa3b382ca557eaed930ead2ffdb
William Trelawny <william.trelawny@graylog.com>
2023-12-15 14:24:43 -0500

    rm server.conf config override from tls course config.sh
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- instruqt/tls/scripts/config.sh
+++ instruqt/tls/scripts/config.sh
@@ -32,16 +32,6 @@ mv /certs/*.pem /.ssl
 openssl x509 -inform PEM -in /.ssl/fullchain.pem -out /usr/local/share/ca-certificates/fullchain.crt
 update-ca-certificates
 
-# Modify server.conf:
-sed -i "s/PUBLICDNS/$dns.logfather.org/" /etc/graylog/server/server.conf
-
-systemctl restart graylog-server
-# Wait for Graylog to be accessible before continuing
-while ! curl -s -u 'admin:yabba dabba doo' http://localhost:9000/api/system/cluster/nodes; do
-	printf "\n\nWaiting for Graylog to come online...\n"
-    sleep 5
-done
-
 # Create Graylog Inputs:
 curl -k -u 'admin:yabba dabba doo' -XPOST "http://localhost:9000/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.gelf.http.GELFHttpInput","configuration":{"bind_address":"0.0.0.0","port":12201,"recv_buffer_size":1048576,"number_worker_threads":1,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"enable_bulk_receiving":false,"enable_cors":true,"max_http_chunk_size":65536,"idle_writer_timeout":60,"override_source":null,"charset_name":"UTF-8","decompress_size_limit":8388608},"title":"GELF HTTP","global":true}'
 
