a3e7c2384a1fcdea1d5e55e99135b16a60b2d037
a-zombie <119435230+a-zombie@users.noreply.github.com>
2023-12-19 16:43:40 -0500

    take two, apt fixes

--- instruqt/common/base_setup.sh
+++ instruqt/common/base_setup.sh
@@ -19,7 +19,7 @@ source /etc/profile
 
 # Update all system packages first:
 printf "\n\n$(date)-Installing System Updates\n"
-sudo apt-get upgrade -q -y -u -o Dpkg::Options::="--force-confdef" --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-change-held-packages --allow-unauthenticated
+sudo DEBIAN_FRONTEND=noninteractive apt-get upgrade -q -y -u -o Dpkg::Options::="--force-confdef" --allow-downgrades --allow-remove-essential --allow-change-held-packages --allow-change-held-packages --allow-unauthenticated
 
 #Install common deps:
 printf "\n\n$(date)-Installing common dependencies\n"
