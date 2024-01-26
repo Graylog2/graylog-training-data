687eaad081d99d80f8ab3d199aaeb757159cfe70
Dan McDowell <mcdowellster@gmail.com>
2024-01-26 15:46:59 -0500

    updated init script for instruqt

--- instruqt/common/instruqt_track_setup.sh
+++ instruqt/common/instruqt_track_setup.sh
@@ -32,30 +32,37 @@ echo "export dns=$dns" >> /etc/profile
 
 # Setup Class Information
 # CLASS should be lowercase and match track's folder name in repo:
-CLASS=""
+CLASS="analyst"
 # TITLE should match "pretty" track name in Instruqt and 
 # should use caps and spaces:
-TITLE=""
+TITLE="Graylog Analyst"
 echo "export CLASS=$CLASS" >> /etc/profile
 echo "export TITLE=$TITLE" >> /etc/profile
 
 # Uncomment below if this course needs a Graylog Docker environment:
 # (Note: Setting this var to any value at all is interpreted as "yes")
-#echo "export NEEDS_DOCKER=yes" >> /etc/profile
+echo "export NEEDS_DOCKER=yes" >> /etc/profile
 
 # Cert Decode File
 echo "${cert_pwd}" > /root/.pwd
 
 # Base Apps
 printf "\n\nGrabbing Base Apps"
-apt-get update
-#apt-get install git-svn -y
+sudo apt update
+sudo apt install git -y
 
-# common Scripts  -- moved to init script with git clone
-#printf "\n\nGrabbing Common Scripts"
-#git svn clone "https://github.com/Graylog2/graylog-training-data/trunk/instruqt/common"
-#mv common /common
-#chmod +x /common/*.sh
+# Git Scripts Scripts
+git clone --depth 1 https://github.com/Graylog2/graylog-training-data.git /graylog-training-data
+mkdir -p /$CLASS
+mv /graylog-training-data/instruqt/$CLASS/* /$CLASS/
+sudo chmod +x /$CLASS/scripts/*.sh
+mkdir -p /common/
+mv /graylog-training-data/instruqt/common/* /common/
+sudo chmod +x /common/*.sh
+mkdir -p /certs
+mv /graylog-training-data/certs/* /certs
+rm -rf /graylog-training-data
+# END FIX
 
 # Run Base Install
 printf "\n\nRunning Base Install"
