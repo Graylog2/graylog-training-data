66aa08f75539d50efce605b040f8d3ae9ae23381
Dan McDowell <mcdowellster@gmail.com>
2024-01-31 23:58:25 -0500

    CTF Instruqt. Let's go!

--- instruqt/ctf/scripts/config.sh
+++ instruqt/ctf/scripts/config.sh
@@ -1,143 +1,65 @@
-#load Vars from Strigo
-source /etc/profile
+# Setup OliveTin:
+/common/setup_olivetin.sh
 
-echo "Sleeping for random time up to 30 seconds to prevent DNS records from being over-written in cases of multiple labs starting at once" >> /home/ubuntu/strigosuccess
-sleep $(( ( RANDOM % 30 )  + 1 ))
+# Setup Greynoise:
+/common/setup_greynoise.sh
 
-echo "Running DNS Registration Steps" >> /home/ubuntu/strigosuccess
-dnscount=0
-DNSMatch=false
-#Check for Existing DNS Record
-echo "Checking for existing record, result:" >> /home/ubuntu/strigosuccess
-DNSCheck=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/08be24924fc30f320e7329020986bad2/dns_records?type=CNAME&name=$dns.logfather.org&match=all" -H "X-Auth-Email: $authemail" -H "Authorization: Bearer $apitoken" -H "Content-Type: application/json" | jq -r '.result[]')
-echo $DNSCheck >> /home/ubuntu/strigosuccess
-if [ ! -z "$DNSCheck" ]; then
-    #Check it's CName to see if it matches existing DNS Record
-    echo "Checking if CNAME is the same, result:" >> /home/ubuntu/strigosuccess
-    CName=$(echo $DNSCheck | jq -r '.content')
-    echo "$CName vs $LAB" >> /home/ubuntu/strigosuccess
-    if [[ ! "$CName" == "$LAB" ]]; then
-        #No Match - new DNS record but also need to check for more numbers
-        #Loop through numbers and check for existing  DNS Records
-        echo "Not a match, looping to find unused DNS record" >> /home/ubuntu/strigosuccess
-        until [[ -z "$DNSCheck" ]];
-        do
-            #Add one to dnscount and check if that record exists. This will loop until null OR a matched CName is found in cases of paused labs THIS causes issues so lets fix it!
-            ((dnscount++))
-            DNSCheck=$(curl -s -X GET "https://api.cloudflare.com/client/v4/zones/08be24924fc30f320e7329020986bad2/dns_records?type=CNAME&name=$dns$dnscount.logfather.org&match=all" -H "X-Auth-Email: $authemail" -H "Authorization: Bearer $apitoken" -H "Content-Type: application/json" | jq -r '.result[]')
-            CName=$(echo $DNSCheck | jq -r '.content')
-            echo "Comparing new DNS record's CNAME (if there is one), result" >> /home/ubuntu/strigosuccess
-            echo $CName >> /home/ubuntu/strigosuccess
-            if [[ "$CName" == "$LAB" ]]; then
-                echo "$CName compared to $LAB is true..." 
-                #If these match, ever, at all, exit the loop and set the DNS record below
-                DNSMatch="true"
-                break
-            fi
-        done
-    fi
-fi
-#ONLY Update DNS Name IF there IS a count. Otherwise everyone is a zero. We don't like that.
-if [ $dnscount -gt 0 ]; then
-    dns="$dns$dnscount"
-fi
+# Setup Maxmind GeoIP databases:
+/common/setup_geoip.sh
 
-#Only create a new DNS record if there isn't one already with a matching CName
-if [[ ! "$DNSMatch" == "true" ]]; then
-    #Create DNS Record
-    echo "Creating DNS Record for: $dns" >> /home/ubuntu/strigosuccess
-    cdata="{\"type\":\"CNAME\",\"name\":\"$dns\",\"content\":\"$LAB\",\"ttl\":3600,\"priority\":10,\"proxied\":false}"
-    createcname=$(curl -s -X POST "https://api.cloudflare.com/client/v4/zones/08be24924fc30f320e7329020986bad2/dns_records" -H "X-Auth-Email: $authemail" -H "Authorization: Bearer $apitoken" -H "Content-Type: application/json" --data $cdata)
-    result=$(echo $createcname | jq '.success')
-    echo "Result: $result" >> /home/ubuntu/strigosuccess
-fi
-echo $dns >> /home/ubuntu/DNSSuccess
-echo "Registered DNS record: $dns"
-sed -i '/export dns=/d' /etc/profile
-echo "export dns=$dns" >> /etc/profile
+#LogData
+mkdir -p /etc/graylog/log_data
+mv "/$CLASS/log_data/" /etc/graylog/
 
-###Cert Update
-echo "Grabbing Certs" >> /home/ubuntu/strigosuccess
-apt install git-svn -y
-#Certs
-git svn clone "https://github.com/Graylog2/graylog-training-data/trunk/certs" >> /home/ubuntu/strigosuccess
-echo "The present working directory is $(pwd)" >> /home/ubuntu/strigosuccess
+#Move Log Generating Scripts
+mv "/$CLASS/scripts/nerdy_log_gen.sh" /etc/graylog/log_data
+chmod +x /etc/graylog/log_data/nerdy_log_gen.sh
 
-## Copy Certs and Decode
-echo "Decoding Certs" >> /home/ubuntu/strigosuccess
-openssl enc -in /certs/privkey.pem.enc -aes-256-cbc -pbkdf2 -d -pass file:/.pwd > /etc/graylog/privkey.pem
-openssl enc -in /certs/cert.pem.enc -aes-256-cbc -pbkdf2 -d -pass file:/.pwd > /etc/graylog/cert.pem
-openssl enc -in /certs/fullchain.pem.enc -aes-256-cbc -pbkdf2 -d -pass file:/.pwd > /etc/graylog/fullchain.pem
-rm /.pwd
-cp /certs/cacerts /etc/graylog/cacerts 
+#Update OT Config 
+echo "Updating OT"
+mv "/$CLASS/configs/olivetin/config.yaml" /etc/OliveTin/config.yaml
+systemctl restart OliveTin.service
 
-#Cert Permissions
-chown root.root /etc/graylog/*.pem
-chmod 600 /etc/graylog/*.pem
+#Creating Indices
+printf "\n\n$(date)-Create General Desktop Events Index\n"
+curl -u 'admin:yabba dabba doo' -XPOST 'http://localhost:9000/api/system/indices/index_sets' -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"title":"General Desktop Events","description":"General Desktop Events","index_prefix":"general-desktop","writable":true,"can_be_default":true,"shards":2,"replicas":0,"retention_strategy_class":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategy","retention_strategy":{"max_number_of_indices":3,"type":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategyConfig"},"index_analyzer":"standard","index_optimization_max_num_segments":1,"index_optimization_disabled":false,"field_type_refresh_interval":5000,"rotation_strategy_class":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategy","rotation_strategy":{"max_docs_per_index":20000,"type":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategyConfig"},"creation_date":"2022-08-17T21:06:47.393Z"}'
+printf "\n\n$(date)-Create Training Index\n"
+curl -u 'admin:yabba dabba doo' -XPOST 'http://localhost:9000/api/system/indices/index_sets' -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"title":"Training","description":"Training","index_prefix":"train","writable":true,"can_be_default":true,"shards":2,"replicas":0,"retention_strategy_class":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategy","retention_strategy":{"max_number_of_indices":3,"type":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategyConfig"},"index_analyzer":"standard","index_optimization_max_num_segments":1,"index_optimization_disabled":false,"field_type_refresh_interval":5000,"rotation_strategy_class":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategy","rotation_strategy":{"max_docs_per_index":20000,"type":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategyConfig"},"creation_date":"2022-08-17T21:06:47.393Z"}'
 
-echo "Updating Keystore" >> /home/ubuntu/strigosuccess
-keytool -import -trustcacerts -alias letsencryptcaroot  -file /etc/graylog/fullchain.pem -keystore /etc/graylog/cacerts -storepass changeit -noprompt >> /home/ubuntu/strigosuccess
+#Cert Injection
+/common/certs.sh
 
-cp /etc/graylog/fullchain.pem /usr/local/share/ca-certificates/fullchain.crt
-update-ca-certificates
+#Course Settings
+/common/course_settings.sh 
 
-## Update Docker Container with certs
-glc=$(sudo docker ps | grep graylog-enterprise | awk '{print $1}')
-docker cp /etc/graylog/cert.pem $glc:/usr/share/graylog/data/config/cert.pem
-docker cp /etc/graylog/privkey.pem $glc:/usr/share/graylog/data/config/privkey.pem
-docker cp /etc/graylog/fullchain.pem $glc:/usr/share/graylog/data/config/fullchain.pem
-docker cp /etc/graylog/cacerts $glc:/usr/share/graylog/data/config/cacerts
+#Add course CPs
+/common/cp_inst.sh 
 
-docker exec -u root -i $glc chown graylog.graylog /usr/share/graylog/data/config/cert.pem
-docker exec -u root -i $glc chown graylog.graylog /usr/share/graylog/data/config/privkey.pem
-docker exec -u root -i $glc chown graylog.graylog /usr/share/graylog/data/config/fullchain.pem
-docker exec -u root -i $glc chown graylog.graylog /usr/share/graylog/data/config/cacerts
+#OT Theme
+/common/ot_gl_theme.sh 
 
 #Update GL Docker Environment
-echo "Removing old variables" >> /home/ubuntu/strigosuccess
-sed -i '/GLEURI=/d' /etc/graylog/strigo-graylog-training-changes.env
-sed -i '/GLBINDADDR=/d' /etc/graylog/strigo-graylog-training-changes.env
-sed -i '/GLTLS=/d' /etc/graylog/strigo-graylog-training-changes.env
+## After this point everything will be HTTPS
+/common/docker_graylog_https.sh
 
-echo "Adding updated variables" >> /home/ubuntu/strigosuccess
-echo "GLBINDADDR=\"0.0.0.0:443\"" >> /etc/graylog/strigo-graylog-training-changes.env
-echo "GLTLS=true" >> /etc/graylog/strigo-graylog-training-changes.env
-echo "GLEURI=https://$dns.logfather.org/" >> /etc/graylog/strigo-graylog-training-changes.env
+#Illuminate Install - moved to POST docker update. Illuminate doesn't seem to fetch first time graylog runs
+/common/inst_illuminate.sh 
 
-#Launch Docker to load changes in env file
-echo "Running Docker Compose to update GL environment with new information" >> /home/ubuntu/strigosuccess
-pwsh -c 'write-host "loaded PS!"'
-
-#Set up log shipping stuffs
-echo "Installing ncat" >> /home/ubuntu/strigosuccess
-sudo apt install ncat -y >> /home/ubuntu/strigosuccess
-
-#Update Docker Config for new OT Ports
-echo "Adding required inputs to GL" >> /home/ubuntu/strigosuccess
-sed -i '/^      - "12201:12201\/udp" # GELF UDP.*/a\      - "5555:5555\/tcp"   # Raw TCP' /etc/graylog/docker-compose-glservices.yml
-sed -i '/^      - "12201:12201\/udp" # GELF UDP.*/a\      - "1514:1514\/tcp"   # Syslog TCP' /etc/graylog/docker-compose-glservices.yml
-
-#Update Graylog Container
-docker compose -f /etc/graylog/docker-compose-glservices.yml --env-file /etc/graylog/strigo-graylog-training-changes.env up -d
+#Install ncat
+sudo apt install ncat -y
 
 #Load Abe's SwapShop!
-echo "Setup docker containers" >> /home/ubuntu/strigosuccess
-docker run -p 7000:80 -d --restart always --log-driver gelf --log-opt gelf-address=tcp://localhost:12201 blueteamninja/swagshop >> /home/ubuntu/strigosuccess
+echo "Setup docker containers"
+docker run -p 7000:80 -d --restart always --log-driver gelf --log-opt gelf-address=tcp://localhost:12201 blueteamninja/swagshop
 
 #Load Abe's Webhook Tester
-docker run -p 8080:8080 -d --restart always tarampampam/webhook-tester >> /home/ubuntu/strigosuccess
-
-#Wait for GL before api calls
-while ! curl -s -k -u 'admin:yabba dabba doo' https://localhost/api/system/cluster/nodes; do
-	printf "\n\nWaiting for GL to come online to add content\n" >> /home/ubuntu/strigosuccess
-    sleep 5
-done
+docker run -p 8080:8080 -d --restart always tarampampam/webhook-tester
 
 #Rick's Sysadmin stuffs!
-echo "Setup The Graybeard" >> /home/ubuntu/strigosuccess
+echo "Setup The Graybeard" 
 # Add a flag to the home directory passwords.txt
-echo 'Who stored this flag in a password document?: Incredibly#Bad#Password#Security' > /home/ubuntu/passwords.txt
-echo 'root@multivac - AlexanderAdell' >> /home/ubuntu/passwords.txt
+echo 'Who stored this flag in a password document?: Incredibly#Bad#Password#Security' > /home/$LUSER/passwords.txt
+echo 'root@multivac - AlexanderAdell' >> /home/$LUSER/passwords.txt
 chown ubuntu:ubuntu /home/ubuntu/passwords.txt
 # get the docker bridge so we can add the lxc container to it, pretty sure this is illegal in 12 states
 docker_bridge="br-"$(docker network list | grep 'graylog_default' | cut -f 1 -d ' ')
@@ -184,63 +106,26 @@ network:
 EOF
 # setup hosts file resolution for graylog container in multivac
 graylog_ip=$(docker container inspect -f '{{ .NetworkSettings.Networks.graylog_default.IPAddress }}' graylog-graylog-1)
-sed -i "s/ZZZZZGRAYLOGIPZZZZZ/$graylog_ip/" /$STRIGO_CLASS_ID/scripts/multivac_config.sh
+sed -i "s/ZZZZZGRAYLOGIPZZZZZ/$graylog_ip/" /$CLASS/scripts/multivac_config.sh
 #echo "$graylog_ip graylog" >> /var/snap/lxd/common/lxd/containers/multivac/rootfs/etc/hosts
 echo "172.18.10.10 multivac" >> /etc/hosts
 # start multivac!
-echo "Starting the Graybeard LXC" >> /home/ubuntu/strigosuccess
-lxc start multivac >> /home/ubuntu/strigosuccess
+echo "Starting the Graybeard LXC" 
+lxc start multivac 
 # execute multivac config script
 sidecar_user=$(curl -s -k -u 'admin:yabba dabba doo' "https://localhost/api/users?include_permissions=false&include_sessions=false" | jq -r '.users[] | select (.username=="graylog-sidecar") | .id')
 sidecar_api=$(curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/users/$sidecar_user/tokens/ctf" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' | jq -r .token)
-sed -i "s/ZZZZZTOKENTOKENZZZZZ/$sidecar_api/" /$STRIGO_CLASS_ID/scripts/multivac_config.sh
-lxc exec multivac -- bash -c "$(cat /$STRIGO_CLASS_ID/scripts/multivac_config.sh)"
+sed -i "s/ZZZZZTOKENTOKENZZZZZ/$sidecar_api/" /$CLASS/scripts/multivac_config.sh
+lxc exec multivac -- bash -c "$(cat /$CLASS/scripts/multivac_config.sh)"
 
-#Add GL inputs
-echo "Adding inputs to Graylog via API" >> /home/ubuntu/strigosuccess
-curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.raw.tcp.RawTCPInput","configuration":{"bind_address":"0.0.0.0","port":5555,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":false,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8"},"title":"RAW Data","global":true,"node":"93f01a3f-d051-436f-9bab-0c11f22cd55c"}'
-curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.syslog.tcp.SyslogTCPInput","configuration":{"bind_address":"0.0.0.0","port":1514,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":false,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8"},"title":"Syslog Data","global":true,"node":"93f01a3f-d051-436f-9bab-0c11f22cd55c"}'
-
-#Move Log Data
-mv "/$STRIGO_CLASS_ID/log_data/" /etc/graylog/
-
-#Move Log Generating Scripts
-mv "/$STRIGO_CLASS_ID/scripts/nerdy_log_gen.sh" /etc/graylog/log_data
-chmod +x /etc/graylog/log_data/nerdy_log_gen.sh
-
-#Update OT Config 
-echo "Updating OT" >> /home/ubuntu/strigosuccess
-mv "/$STRIGO_CLASS_ID/configs/olivetin/config.yaml" /etc/OliveTin/config.yaml
-systemctl restart OliveTin.service
-
-#Add course CPs
-for entry in /$STRIGO_CLASS_ID/configs/content_packs/*
-do
-  printf "\n\nInstalling Content Package: $entry\n" >> /home/ubuntu/strigosuccess
-  id=$(cat "$entry" | jq -r '.id')
-  ver=$(cat "$entry" | jq -r '.rev')
-  printf "\n\nID:$entry and Version: $ver\n" >> /home/ubuntu/strigosuccess
-  curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/content_packs"  -H 'Content-Type: application/json' -H 'X-Requested-By: PS_Packer' -d @"$entry"
-  printf "\n\nEnabling Content Package: $entry\n" >> /home/ubuntu/strigosuccess
-  curl -k -u'admin:yabba dabba doo' -XPOST "https://localhost/api/system/content_packs/$id/$ver/installations" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"parameters":{},"comment":""}'
+#Wait for GL before api calls
+while ! curl -s -k -u 'admin:yabba dabba doo' https://localhost/api/system/cluster/nodes; do
+	printf "\n\nWaiting for GL to come online to add content\n" 
+    sleep 5
 done
 
-#Setup Illuminate using API
-printf "\n\nInstalling Illuminate" >> /home/ubuntu/strigosuccess
-ilver=$(curl -u 'admin:yabba dabba doo' -k -XGET 'https://localhost/api/plugins/org.graylog.plugins.illuminate/bundles/hub/latest' | jq -r '.version')
-printf "\n\nFound Illuminate Version:$ilver\n" >> /home/ubuntu/strigosuccess
-ilinst=$(curl -u 'admin:yabba dabba doo' -k -XPOST "https://localhost/api/plugins/org.graylog.plugins.illuminate/bundles/hub/$ilver" -k -H 'X-Requested-By: PS_TeamAwesome')
-printf "\n\nDownload Version $ilver - result: $ilinst\n" >> /home/ubuntu/strigosuccess
-bunact=$(curl -u 'admin:yabba dabba doo' -k -XPOST "https://localhost/api/plugins/org.graylog.plugins.illuminate/bundles/$ilver" -k -H 'X-Requested-By: PS_TeamAwesome')
-printf "\n\nInstallation Result: $bunact\n" >> /home/ubuntu/strigosuccess
-
-#Cleanup this folder so noones cheaters
-echo "Cleanup" >> /home/ubuntu/strigosuccess
-sed -i '/export apitoken=/d' /etc/profile
-sed -i '/export authemail=/d' /etc/profile
-rm -r /certs
-rm -r /$STRIGO_CLASS_ID
-
-#Create file for lab to finally appear
-touch /home/ubuntu/gogogo
-echo "Complete!" >> /home/ubuntu/strigosuccess
+#Add GL inputs
+echo "Adding inputs to Graylog via API" 
+curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.raw.tcp.RawTCPInput","configuration":{"bind_address":"0.0.0.0","port":5555,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":false,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8"},"title":"RAW Data","global":true,"node":"93f01a3f-d051-436f-9bab-0c11f22cd55c"}'
+curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.syslog.tcp.SyslogTCPInput","configuration":{"bind_address":"0.0.0.0","port":1514,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":false,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8"},"title":"Syslog Data","global":true,"node":"93f01a3f-d051-436f-9bab-0c11f22cd55c"}'
+curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.gelf.tcp.GELFTCPInput","configuration":{"bind_address":"0.0.0.0","port":12201,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":true,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8","decompress_size_limit":8388608},"title":"GELF TCP","global":true,"node":"3f46b40b-8399-46ad-80da-2deed2c29bd6"}'
