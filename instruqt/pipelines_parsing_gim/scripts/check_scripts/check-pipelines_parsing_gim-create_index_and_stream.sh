d996a0d8cd9cc7b40d9c13406173ea9cf25f85c7
Dan McDowell <mcdowellster@gmail.com>
2024-03-07 15:02:44 -0500

    Check updated for index

--- instruqt/pipelines_parsing_gim/scripts/check_scripts/check-pipelines_parsing_gim-create_index_and_stream.sh
+++ instruqt/pipelines_parsing_gim/scripts/check_scripts/check-pipelines_parsing_gim-create_index_and_stream.sh
@@ -1,3 +1,8 @@
+index=$(curl -u 'admin:yabba dabba doo' -k -XGET "https://localhost/api/system/indexer/indices" | jq '.all.indices[] | select(.index_name=="mfi_0") | .index_name')
+if [ -z "$id" ];then 
+    fail-message "Oops, it looks like you still need create your first index";  
+fi
+
 id=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/streams" | jq -r '.streams | .[] | select(.title=="My First Stream") | .id')
 if [ -z "$id" ];then 
     fail-message "Oops, it looks like you still need create your first stream";  
