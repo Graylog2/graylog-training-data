6f77bfa1ad0e72f67709c435454dc6eca41d264b
Dan McDowell <mcdowellster@gmail.com>
2024-03-07 15:05:04 -0500

    scripts need exit 0 for each check

--- instruqt/pipelines_parsing_gim/scripts/check_scripts/check-pipelines_parsing_gim-create_index_and_stream.sh
+++ instruqt/pipelines_parsing_gim/scripts/check_scripts/check-pipelines_parsing_gim-create_index_and_stream.sh
@@ -1,9 +1,11 @@
 index=$(curl -u 'admin:yabba dabba doo' -k -XGET "https://localhost/api/system/indexer/indices" | jq '.all.indices[] | select(.index_name=="mfi_0") | .index_name')
 if [ -z "$id" ];then 
-    fail-message "Oops, it looks like you still need create your first index";  
+    fail-message "Oops, it looks like you still need create your first index"; 
+    exit 0 
 fi
 
 id=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/streams" | jq -r '.streams | .[] | select(.title=="My First Stream") | .id')
 if [ -z "$id" ];then 
-    fail-message "Oops, it looks like you still need create your first stream";  
+    fail-message "Oops, it looks like you still need create your first stream";
+    exit 0
 fi
--- instruqt/pipelines_parsing_gim/scripts/check_scripts/check-pipelines_parsing_gim-create_pipeline_and_rule.sh
+++ instruqt/pipelines_parsing_gim/scripts/check_scripts/check-pipelines_parsing_gim-create_pipeline_and_rule.sh
@@ -1,15 +1,18 @@
 pipeid=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/system/pipelines/pipeline" | jq -r '.[] | select(.title=="Routing") | .id')
 if [ -z "$pipeid" ];then 
-    fail-message "Oops, it looks like you still need create your pipeline!";  
+    fail-message "Oops, it looks like you still need create your pipeline!";
+    exit 0
 fi
 
 ruleid=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/system/pipelines/rule" | jq -r '.[] | select(.title=="Route - My First Stream Logs") | .id')
 if [ -z "$ruleid" ];then 
-    fail-message "Oops, it looks like you still need create your routing rule!";  
+    fail-message "Oops, it looks like you still need create your routing rule!";
+    exit 0
 fi
 
 pljson=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/system/pipelines/pipeline/$pipeid")
 plrules=$(echo $pljson | jq -r '.stages | .[].rules')
 if [[ $plrules != *"Route - My First Stream Logs"* ]]; then
     fail-message "Oops, it looks like you still need to add your routing rule to your pipeline"
+    exit 0
 fi
