f15b9e8a32bf83b342593508e5141685a19b16da
mcdowellster <mcdowellster@gmail.com>
2024-03-27 13:53:43 -0400

    grok checks and solve

---
+++ instruqt/pipelines_parsing_gim/scripts/check_scripts/check-pipelines_parsing_gim-parsing-grok.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Set script to exit on any non-zero exit code and display extra debug info (per Instruqt's recommendation):
+set -exo pipefail
+# Import env vars used throughout scripts runtime
+source /etc/profile
+
+pipeid=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/system/pipelines/pipeline" | jq -r '.[] | select(.title=="Desktop Firewalls") | .id')
+if [ -z "$pipeid" ];then 
+    fail-message "Oops, it looks like you still need create your pipeline!";
+    exit 0
+fi
+
+ruleid=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/system/pipelines/rule" | jq -r '.[] | select(.title=="Parse - Firewall - GROK") | .id')
+if [ -z "$ruleid" ];then 
+    fail-message "Oops, it looks like you still need create your GROK rule!";
+    exit 0
+fi
+
+pljson=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/system/pipelines/pipeline/$pipeid")
+plrules=$(echo $pljson | jq -r '.stages | .[].rules')
+if [[ $plrules != *"Parse - Firewall - GROK"* ]]; then
+    fail-message "Oops, it looks like you still need to add your rule to your pipeline"
+    exit 0
+fi
+
+rule=$(curl -X GET "localhost:9200/_search?size=1" -H 'Content-Type: application/json' -d '{"query":{"exists":{"field":"rule_demo"}}}' | jq '.hits.hits[]._index')
+if [ -z "$rule" ]; then
+    fail-message "Oops, it looks like no logs have the rule_demo field added! Go check your pipeline / resend logs"
+    exit 0
+fi
---
+++ instruqt/pipelines_parsing_gim/scripts/solve_scripts/solve-pipelines_parsing_gim-check-parsing-grok.sh
@@ -0,0 +1,7 @@
+#Get Content Pack
+wget https://github.com/Graylog2/graylog-training-data/raw/main/instruqt/pipelines_parsing_gim/scripts/solve_content_packs/solve-pipelines_parsing_gim-pipelines-grok.json
+
+id=$(cat solve-pipelines_parsing_gim-pipelines-grok.json | jq -r '.id')
+ver=$(cat solve-pipelines_parsing_gim-pipelines-grok.json | jq -r '.rev')
+curl -u 'admin:yabba dabba doo' -k -XPOST "https://localhost/api/system/content_packs"  -H 'Content-Type: application/json' -H 'X-Requested-By: PS_Packer' -d @"solve-pipelines_parsing_gim-pipelines-grok.json" 
+curl -u'admin:yabba dabba doo' -k -XPOST "https://localhost/api/system/content_packs/$id/$ver/installations" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"parameters":{},"comment":""}' 
