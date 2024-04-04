98a01d7b445755985c0fba03f673401fce2e5c9e
mcdowellster <mcdowellster@gmail.com>
2024-04-04 14:49:06 -0400

    typo

--- instruqt/pipelines_parsing_gim/scripts/solve_scripts/solve-pipelines_parsing_gim-check-parsing-kvp.sh
+++ instruqt/pipelines_parsing_gim/scripts/solve_scripts/solve-pipelines_parsing_gim-check-parsing-kvp.sh
@@ -13,6 +13,6 @@ pipeid=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/system
 wget https://github.com/Graylog2/graylog-training-data/raw/main/instruqt/pipelines_parsing_gim/scripts/solve_content_packs/solve-pipelines_parsing_gim-kvp_pipeline.json
 
 #update pipelineID with sed
-sed -i "s/660eec61faf32e49a3b50f45/$pipeid/g" solve-pipelines_parsing_gim-create_pipeline_and_rule.json
+sed -i "s/660eec61faf32e49a3b50f45/$pipeid/g" solve-pipelines_parsing_gim-kvp_pipeline.json
 curl -XPUT -u 'admin:yabba dabba doo' -k "https://localhost/api/system/pipelines/pipeline/$pipeid" -H 'Content-Type: application/json' -H 'X-Requested-By: skipper' -d @"solve-pipelines_parsing_gim-kvp_pipeline.json"
 
