75707f8970516c7dfe2e2c57600ebf01ae82c7ba
William Trelawny <william.trelawny@graylog.com>
2024-04-09 09:24:40 -0400

    added webhook tester and maildev containers to Events course
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- instruqt/events/scripts/config.sh
+++ instruqt/events/scripts/config.sh
@@ -10,7 +10,7 @@ source /etc/profile
 # and to potentially help with Events timing issues.
 # GELF Spec says timestamp should be Unix epoch seconds, optionally plus microseconds as decimals.
 # ref: GELF Payload Specification: https://archivedocs.graylog.org/en/latest/pages/gelf.html#gelf-payload-specification
-sed -i 's/  /root/.config/powershell/Microsoft.PowerShell_profile.ps1
+#sed -i 's/  /root/.config/powershell/Microsoft.PowerShell_profile.ps1'
 
 # Setup Greynoise:
 /common/setup_greynoise.sh
@@ -18,34 +18,40 @@ sed -i 's/  /root/.config/powershell/Microsoft.PowerShell_profile.ps1
 # Setup Maxmind GeoIP databases:
 /common/setup_geoip.sh
 
-#LogData
+# LogData
 sudo mv /$CLASS/log_data/* /root/powershell/Data
 sudo chown -R root.root /root
 sudo mkdir -p /etc/graylog/log_data
 sudo mv /root/powershell/Data/firewall.log /etc/graylog/log_data/
 sudo mv /root/powershell/Data/kvp.log /etc/graylog/log_data/
 
-#Creating Indices
+# Creating Indices
 printf "\n\n$(date)-Create Training Index\n"
 curl -u 'admin:yabba dabba doo' -XPOST 'http://localhost:9000/api/system/indices/index_sets' -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"title":"Training","description":"Training","index_prefix":"train","writable":true,"can_be_default":true,"shards":2,"replicas":0,"retention_strategy_class":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategy","retention_strategy":{"max_number_of_indices":3,"type":"org.graylog2.indexer.retention.strategies.DeletionRetentionStrategyConfig"},"index_analyzer":"standard","index_optimization_max_num_segments":1,"index_optimization_disabled":false,"field_type_refresh_interval":5000,"rotation_strategy_class":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategy","rotation_strategy":{"max_docs_per_index":20000,"type":"org.graylog2.indexer.rotation.strategies.MessageCountRotationStrategyConfig"},"creation_date":"2022-08-17T21:06:47.393Z"}'
 
-#Cert Injection
+# Cert Injection
 /common/certs.sh
 
-#Course Settings
+# Course Settings
 /common/course_settings.sh 
 
-#Add course CPs
+# Add course CPs
 /common/cp_inst.sh 
 
 #OT Theme
 /common/ot_gl_theme.sh 
 
-#Update GL Docker Environment
+# Update GL Docker Environment
 ## After this point everything will be HTTPS
 /common/docker_graylog_https.sh
 
-#Illuminate Install - moved to POST docker update. Illuminate doesn't seem to fetch first time graylog runs
+# Illuminate Install - moved to POST docker update. Illuminate doesnt seem to fetch first time graylog runs
 /common/inst_illuminate.sh 
+
+# Deploy webhook test container:
+docker run -p 8080:8080 -d --restart always tarampampam/webhook-tester
+
+# Deploy maildev container for SMTP testing:
+docker run -p 1080:1080 -p 1025:1025 maildev/maildev
 
 echo "Complete!" 
