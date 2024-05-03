9cac3215fbf7ac8e1a14cdab5c3f27c4fd78d982
mcdowellster <mcdowellster@gmail.com>
2024-05-03 09:54:52 -0400

    CP fixes

--- instruqt/context_enrichment/scripts/setup_scripts/setup-context_enrichment-context_challenge.sh
+++ instruqt/context_enrichment/scripts/setup_scripts/setup-context_enrichment-context_challenge.sh
@@ -4,6 +4,16 @@ set -exo pipefail
 # Import env vars used throughout scripts runtime
 source /etc/profile
 
+#Add Desktop Firewall Events Stream
+##Get Index ID
+indexID=$(curl -u 'admin:yabba dabba doo' -k -XGET "https://localhost/api/system/indices/index_sets?skip=0&limit=0&stats=false" | jq -r '.index_sets[] | select (.title=="General Desktop Events").id')
+##Add Stream
+curl -u 'admin:yabba dabba doo' -k -XPOST "https://localhost/api/streams"  -H 'Content-Type: application/json' -H 'X-Requested-By: PS_Packer' -d "{\"index_set_id\":\"$indexID\",\"description\":\"\",\"title\":\"Desktop Firewall Events\",\"remove_matches_from_default_stream\":false}"
+
+##Start Stream
+id=$(curl -u 'admin:yabba dabba doo' -k -XGET  "https://localhost/api/streams" | jq -r '.streams | .[] | select(.title=="Desktop Firewall Events") | .id')
+curl -u 'admin:yabba dabba doo' -k -XPOST "https://localhost/api/streams/$id/resume" -H 'X-Requested-By: Skipper'
+
 #Get Content Pack
 wget https://github.com/Graylog2/graylog-training-data/raw/main/instruqt/context_enrichment/scripts/setup_scripts/setup-context_enrichment-context_challenge.json
 
