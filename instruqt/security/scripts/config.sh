39550b9d55aa1c744cfe4f821b7ca0e508604daf
neddy0graylog <ned.dunningham@graylog.com>
2024-08-15 14:23:54 +0100

    bones of security course

---
+++ instruqt/security/scripts/config.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+# Import env vars used throughout scripts runtime
+source /etc/profile
+
+# Setup OliveTin:
+/common/setup_olivetin.sh
+
+# Setup Greynoise:
+/common/setup_greynoise.sh
+
+# Setup Maxmind GeoIP databases:
+/common/setup_geoip.sh
+
+#LogData
+sudo mv /$CLASS/log_data/* /root/powershell/Data
+sudo chown -R root.root /root
+
+# Event Replay
+chmod +x /$CLASS/apps/event_replay/installer/install.sh
+/$CLASS/apps/event_replay/installer/install.sh
+
+#Cert Injection
+/common/certs.sh
+
+#Course Settings
+/common/course_settings.sh 
+
+#Add course CPs
+/common/cp_inst.sh 
+
+#OT Theme
+/common/ot_gl_theme.sh 
+
+#Update GL Docker Environment
+## After this point everything will be HTTPS
+/common/docker_graylog_https.sh
+
+#Illuminate Install - moved to POST docker update. Illuminate doesn't seem to fetch first time graylog runs
+/common/inst_illuminate.sh 
+
+echo "Complete!" 
