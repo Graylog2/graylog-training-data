e708a5fadc6a516b781ac96b72c409d797aa36e9
William Trelawny <william.trelawny@graylog.com>
2025-02-11 10:40:17 -0500

    update tls course's generate_certs.sh script to exclude opensearch files
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- instruqt/tls/scripts/generate_certs.sh
+++ instruqt/tls/scripts/generate_certs.sh
@@ -9,22 +9,12 @@ sleep 4
 [[ ! -d /root/certs ]] && mkdir /root/certs
 cd /.ssl
 cp cert.pem /root/certs/graylog.pem
-cp cert.pem /root/certs/opensearch.pem
 cp privkey.pem /root/certs/graylog.key
-cp privkey.pem /root/certs/opensearch.key
 cp fullchain.pem /root/certs/fullchain.pem
-cp osadmin_cert.pem /root/certs/osadmin.pem
-cp osadmin_privkey.pem /root/certs/osadmin.key
-cp osadmin_fullchain.pem /root/certs/osadmin_fullchain.pem
 chmod 0400 /root/certs/*.key
 
 printf "\n\e[1;37m=== Certificates signed successfully!\e[0m\n"
 printf "\n=== Your certificate files & private keys have been uploaded to: \e[1;93m/root/certs\e[0m\n\n"
 printf "      \e[0;31mGraylog server cert ......... graylog.pem\e[0m\n"
 printf "      \e[0;31mGraylog server key .......... graylog.key\e[0m\n"
-printf "      \e[0;34mOpensearch server cert ...... opensearch.pem\e[0m\n"
-printf "      \e[0;34mOpensearch server key ....... opensearch.key\e[0m\n"
 printf "      \e[0;33mCertificate chain ........... fullchain.pem\e[0m\n\n"
-printf "      \e[0;36mOpensearch admin user cert .. osadmin.pem\e[0m\n"
-printf "      \e[0;36mOpensearch admin user key ... osadmin.key\e[0m\n"
-printf "      \e[0;33mOpensearch admin fullchain .. osadmin_fullchain.pem\e[0m\n"
