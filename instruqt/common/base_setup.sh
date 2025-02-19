e94973ac57194ce2e2a439f397616b2ecab58730
William Trelawny <william.trelawny@graylog.com>
2025-02-19 11:32:01 -0500

    minor update base_setup.sh
    - Changed "Your public DNS record is" to "Your public URL is" for better clarity
    
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- instruqt/common/base_setup.sh
+++ instruqt/common/base_setup.sh
@@ -40,7 +40,7 @@ printf "\e[37m╚██████╔╝██║  ██║██║  ██�
 printf "\e[37m ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   \e[31m╚══════╝ ╚═════╝  ╚═════╝ \n";
 printf "                                                                        \n";
 printf "\e[39m Hi,\n Welcome to ${TITLE}!\n"
-printf "\e[93m Your public DNS record is: https://$dns.logfather.org\n";
+printf "\e[93m Your public URL is: https://$dns.logfather.org\n";
 
 EOF
 
