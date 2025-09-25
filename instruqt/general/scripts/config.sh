e6ba4ddadceff18e9c6c50bce720017761185b82
William Trelawny <william.trelawny@graylog.com>
2025-09-25 09:01:41 -0400

    Update for BD Training Day 3
    Signed-off-by: William Trelawny <william.trelawny@graylog.com>

--- instruqt/general/scripts/config.sh
+++ instruqt/general/scripts/config.sh
@@ -44,4 +44,7 @@ curl -u 'admin:yabba dabba doo' -XPOST 'http://localhost:9000/api/system/indices
 #Illuminate Install - moved to POST docker update. Illuminate doesn't seem to fetch first time graylog runs
 /common/inst_illuminate.sh 
 
+# Create input for Day 3 Lab 1:
+curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.syslog.tcp.SyslogTCPInput","configuration":{"bind_address":"0.0.0.0","port":514,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":false,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8","force_rdns":false,"allow_override_date":true,"store_full_message":false,"expand_structured_data":false,"timezone":"NotSet"},"title":"Day 3 Lab","global":true,"node":"42397cba-d8b3-444a-b818-a4dc4213511e"}'
+
 echo "Complete!" 
