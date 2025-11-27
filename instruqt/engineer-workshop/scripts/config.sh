450154d8935c6aec02d94b8eff41e723bf309dfe
a-zombie <119435230+a-zombie@users.noreply.github.com>
2025-11-27 11:27:37 -0500

    create gelf input in eng lab

--- instruqt/engineer-workshop/scripts/config.sh
+++ instruqt/engineer-workshop/scripts/config.sh
@@ -45,13 +45,14 @@ curl -u 'admin:yabba dabba doo' -XPOST 'http://localhost:9000/api/system/indices
 #Illuminate Install - moved to POST docker update. Illuminate doesn't seem to fetch first time graylog runs
 /common/inst_illuminate.sh 
 
-# Create input for Day 3 Lab 1:
-curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.syslog.tcp.SyslogTCPInput","configuration":{"bind_address":"0.0.0.0","port":514,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":false,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8","force_rdns":false,"allow_override_date":true,"store_full_message":false,"expand_structured_data":false,"timezone":"NotSet"},"title":"Day 3 Lab","global":true,"node":"42397cba-d8b3-444a-b818-a4dc4213511e"}'
+# Create GELF input:
+curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.gelf.tcp.GELFTCPInput","configuration":{"bind_address":"0.0.0.0","port":12201,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":true,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8","decompress_size_limit":8388608},"title":"GELF TCP","global":true,"node":"193f0219-bd7d-4635-8eec-6ce7ed9daca9"}'
+#curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/system/inputs" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d '{"type":"org.graylog2.inputs.syslog.tcp.SyslogTCPInput","configuration":{"bind_address":"0.0.0.0","port":514,"recv_buffer_size":1048576,"number_worker_threads":2,"tls_cert_file":"","tls_key_file":"","tls_enable":false,"tls_key_password":"","tls_client_auth":"disabled","tls_client_auth_cert_file":"","tcp_keepalive":false,"use_null_delimiter":false,"max_message_size":2097152,"override_source":null,"charset_name":"UTF-8","force_rdns":false,"allow_override_date":true,"store_full_message":false,"expand_structured_data":false,"timezone":"NotSet"},"title":"Day 3 Lab","global":true,"node":"42397cba-d8b3-444a-b818-a4dc4213511e"}'
 
 # Create Firewall Stream
-curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/streams" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d  '{"index_set_id":"68d5655daf97a6672c363eb1","description":"Firewall Logs","title":"Firewall Stream","remove_matches_from_default_stream":true}'
+#curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/streams" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome' -d  '{"index_set_id":"68d5655daf97a6672c363eb1","description":"Firewall Logs","title":"Firewall Stream","remove_matches_from_default_stream":true}'
 
 # Start new Firewall Stream:
-curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/streams/68d5655daf97a6672c363eb1/resume" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome'
+#curl -k -u 'admin:yabba dabba doo' -XPOST "https://localhost/api/streams/68d5655daf97a6672c363eb1/resume" -H 'Content-Type: application/json' -H 'X-Requested-By: PS_TeamAwesome'
 
 echo "Complete!" 
